0859d281d7df64c9a48c89f8e44e00b8fca9d6f9
Edgar Zapeka <edz@Edgars-MacBook-Pro.local>
2018-03-01 21:29:19 -0800

    Added interface and some functionality

--- CelciusToFahrenheit/CelciusToFahrenheit/ViewController.swift
+++ CelciusToFahrenheit/CelciusToFahrenheit/ViewController.swift
@@ -10,14 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var myLabel: UILabel?
+    @IBOutlet weak var switchTempType: UISegmentedControl!
+    @IBOutlet weak var converterTitle: UILabel?
+    @IBOutlet weak var temperatureTypeTitle: UILabel?
+    @IBOutlet weak var selectedTempTypeLabel: UILabel?
+    @IBOutlet weak var selectedTempTypeValueLabel: UILabel?
+    @IBOutlet weak var convertedTempTypeLabel: UILabel?
+    @IBOutlet weak var convertedTempTypeValueLabel: UILabel?
+    @IBOutlet weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.myLabel?.text = "Temperature Converter"
-        self.myLabel?.font = self.myLabel?.font.withSize(30)
-        self.myLabel?.textAlignment = .center
+        initUI()
+        
+        
+    }
+    
+    @IBAction func switchType(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.selectedTempTypeLabel?.text = "Celcius"
+            self.convertedTempTypeLabel?.text = "Farenheit"
+            calculateTmp()
+        case 1:
+            self.selectedTempTypeLabel?.text = "Farenheit"
+            self.convertedTempTypeLabel?.text = "Celcius"
+            calculateTmp()
+        default:
+            self.selectedTempTypeLabel?.text = "Celcius"
+            self.convertedTempTypeLabel?.text = "Farenheit"
+        }
+    }
+    
+    
+    func initUI(){
+        self.converterTitle?.text = "Temperature Converter"
+        self.converterTitle?.font = self.converterTitle?.font.withSize(30)
+        self.converterTitle?.textAlignment = .center
+        
+        self.temperatureTypeTitle?.text = "Convert:"
+        self.temperatureTypeTitle?.font = self.temperatureTypeTitle?.font.withSize(20)
+        self.temperatureTypeTitle?.textAlignment = .center
+        
+        let titles = ["Celcius", "Farenheit"]
+        self.switchTempType?.setTitle(titles[0], forSegmentAt: 0)
+        self.switchTempType?.setTitle(titles[1], forSegmentAt: 1)
+        
+        self.selectedTempTypeLabel?.text = "Celcius"
+        self.convertedTempTypeLabel?.text = "Farenheit"
+    }
+    
+    @IBAction func handleInput(_ sender: UITextField) {
+        self.selectedTempTypeValueLabel?.text = sender.text
+        calculateTmp()
+    }
+    func calculateTmp(){
+        var inputValue: Float = (self.selectedTempTypeValueLabel?.text as! NSString).floatValue
+        if (self.selectedTempTypeLabel?.text == "Celcius"){
+            self.convertedTempTypeValueLabel?.text = String(inputValue * 9/5 + 32)
+        }else{
+            self.convertedTempTypeValueLabel?.text = String(inputValue * 1.8 + 32 )
+        }
     }
 
     override func didReceiveMemoryWarning() {
