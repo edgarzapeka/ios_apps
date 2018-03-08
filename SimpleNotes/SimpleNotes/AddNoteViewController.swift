ef4dae6b3827ae1f5907f9f577ec58b470d58ef2
Edgar Zapeka <edz@edgars-mbp.wifi.bcit.ca>
2018-03-08 11:43:26 -0800

    Added new Project

---
+++ SimpleNotes/SimpleNotes/AddNoteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddNoteViewController.swift
+//  SimpleNotes
+//
+//  Created by Edgar Zapeka on 2018-03-08.
+//  Copyright © 2018 Edgar Zapeka. All rights reserved.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SimpleNotes/SimpleNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleNotes
+//
+//  Created by Edgar Zapeka on 2018-03-08.
+//  Copyright © 2018 Edgar Zapeka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
